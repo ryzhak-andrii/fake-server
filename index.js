bb4ff32a1bab02dc217782a9ee6f05096854312f
Bohdan Shumakov <bohdanshumakov@gmail.com>
2022-07-06 04:25:11 +0300

    fix(firebase): port conflict

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 // mount routes
 app.use("/firebase", router);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${[port]}`));
 
